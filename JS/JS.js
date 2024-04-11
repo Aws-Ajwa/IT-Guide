5ecc75559a5478509ba807a8b4c8b261ca494f9e
awsajwa <100875404+Aws-Ajwa@users.noreply.github.com>
2024-04-11 05:46:28 +0300

    animation to the main page

---
+++ JS/JS.js
@@ -0,0 +1,46 @@
+//loader
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
+//autoplay
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  focusOnSelect: true,
+  infinite: true,
+  pauseOnHover: true,
+  dots: true,
+});
+//nav bar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//animation
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.square');
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach(h => observer.observe(h));
